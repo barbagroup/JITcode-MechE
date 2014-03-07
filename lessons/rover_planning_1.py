cf21e4cf77eaa08b64a3bbb694da9d9ea4b0d1cc
Alex Golding <alex718@gwmail.gwu.edu>
2014-03-06 22:03:52 -0500

    Tweaked notebook to continue using animation
    Left turning not working quite right; will figure out tomorrow

--- lessons/rover_planning_1.py
+++ lessons/rover_planning_1.py
@@ -1,4 +1,4 @@
-def gorover(student_func):
+def gorover(student_func,maze):
     '''
     student_func should be defined in the iPython notebook namespace and provide directions as text strings, either
     'turn right'
@@ -12,12 +12,6 @@ def gorover(student_func):
     import matplotlib.pyplot as plt
     import matplotlib.animation as animation
 
-    maze02 = np.array([[5,5,5,5,5,5,5,5,5,5],[5,0,0,0,4,0,3,0,0,5],[5,0,0,3,3,3,3,3,3,5]])
-    maze35 = np.array([[5,0,0,3,0,0,3,0,3,5],[5,3,0,3,0,0,3,3,0,5],[5,0,0,3,0,0,3,0,0,5]])
-    maze69 = np.array([[5,0,0,0,0,0,3,3,0,5],[5,0,0,3,0,0,0,1,3,5],[5,3,0,0,0,0,3,0,0,5],[5,5,5,5,5,5,5,5,5,5]])
-    maze = np.append(maze02,maze35,axis = 0)
-    maze = np.append(maze,maze69,axis = 0)
-
     mazehist = maze.copy()
 
     position = np.array([7,7])
@@ -26,32 +20,36 @@ def gorover(student_func):
     right = np.array([[0,1],[-1,0]]) #rotation matrix for turning left 90 degrees
     left = np.array([[0,-1],[1,0]]) #rotation matrix for turning right 90 degrees
     escape = 0
+    numpasses = 0
 #    plt.figure(figsize=(10, 5))
     while escape == 0:
-            maze[position[0],position[1]] = 1
-            if (position[0] == target[0]) and (position[1] == target[1]):
-                    maze[position[0],position[1]] = 1
-                    maze[target[0],target[1]] = 2
-                    escape = 1
-            next = np.add(position,heading)
-            if (maze[next[0],next[1]] ==3) or (maze[next[0],next[1]] == 5):
-                    infront = 'hazard'
-            else:
-                    infront = 'who cares'
-            command = student_func(infront)
-            if command == 'turn right':
-                    heading = right.dot(heading)
-            if command == 'turn left':
-                    heading = left.dot(heading)
-            if command == 'go forward':
-                    position = position + heading
-            if command == 'turn around':
-                    heading = -heading
+			numpasses = numpasses +1
+			maze[position[0],position[1]] = 1
+			if (position[0] == target[0]) and (position[1] == target[1]):
+					maze[position[0],position[1]] = 1
+					maze[target[0],target[1]] = 2
+					escape = 1
+			next = np.add(position,heading)
+			if (maze[next[0],next[1]] ==3) or (maze[next[0],next[1]] == 5):
+					infront = 'hazard'
+			else:
+					infront = 'who cares'
+			command = student_func(infront)
+			if command == 'turn right':
+					heading = right.dot(heading)
+			if command == 'turn left':
+					heading = left.dot(heading)
+			if command == 'go forward':
+					position = position + heading
+			if command == 'turn around':
+					heading = -heading
+			if numpasses == 100:	
+				break
             #plt.imshow(maze, cmap=plt.cm.Reds, interpolation='nearest')
             #plt.xticks([]), plt.yticks([])
             #plt.show()
             #print(position, heading)
-            mazehist = np.dstack((mazehist,maze))
+			mazehist = np.dstack((mazehist,maze))
 
     fig = plt.figure(figsize=(8,4))
 
