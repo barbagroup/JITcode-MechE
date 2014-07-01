f91ec713022891ebe2a88f61338f1d559f7ab89b
Zach Wade <zacharywade@gmail.com>
2014-07-01 12:37:15 -0400

    new readme generator that includes the module

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ JITcode-MechE
 JIT modules to learn computing in a problem-based context within Mechanical Engineering
 
 ###Notebooks
-* [Quick Python Intro](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/lessons/00_Lesson00_Quick_Python_Intro.ipynb)
-* [Playing with data](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/lessons/01_Lesson01_Playing_with_data.ipynb)
-* [Unit Mixups](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/lessons/02_Lesson02_Unit_Mixups.ipynb)
-* [Conditionals](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/lessons/03_Lesson03_Conditionals.ipynb)
-* [Loops](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/lessons/04_Lesson04_Loops.ipynb)
-* [DataAnalysis](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/lessons/05_Lesson05_DataAnalysis.ipynb)
-* [Particle on Parabola](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/lessons/99_Lesson99_Particle_on_Parabola.ipynb)
+####Introduction to Python
+* [Quick Python Intro](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/module00_Introduction_to_Python/00_Lesson00_Quick_Python_Intro.ipynb)
+* [Playing with data](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/module00_Introduction_to_Python/01_Lesson01_Playing_with_data.ipynb)
+* [Unit Mixups](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/module00_Introduction_to_Python/02_Lesson02_Unit_Mixups.ipynb)
+* [Units and Decisions](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/module00_Introduction_to_Python/02_Lesson02_Units_and_Decisions.ipynb)
+* [Conditionals](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/module00_Introduction_to_Python/03_Lesson03_Conditionals.ipynb)
+* [Loops](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/module00_Introduction_to_Python/04_Lesson04_Loops.ipynb)
+* [DataAnalysis](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/module00_Introduction_to_Python/05_Lesson05_DataAnalysis.ipynb)
+* [Particle on Parabola](http://nbviewer.ipython.org/urls/github.com/barbagroup/JITcode-MechE/blob/master/module00_Introduction_to_Python/99_Lesson99_Particle_on_Parabola.ipynb)
--- gen-readme.py
+++ gen-readme.py
@@ -24,17 +24,29 @@ format_item = '* [{name}]({url})'.format
 bb_url = 'github.com/barbagroup/JITcode-MechE/blob/master/{}'.format
 #base url of repo where notebooks are stored
 
+format_module = '####{name}'.format
+#Format string for module headers
+
 def notebooks():
     '''
     get list of all ipython notebook files in lessons subdir
     '''
-    return glob('lessons/[0-9]*.ipynb')
+    return glob('module*/*.ipynb')
 
 def lesson_id(filename):
     '''
     used to sort notebook display order based on prefixed number
     at beginning of filename
     '''
+
+    return int(re.search('/([0-9]+)_', filename).group(1))
+
+def module_id(filename):
+    '''
+    used to sort notebook order based on which module directory
+    the file is found in
+    '''
+
     return int(re.search('[0-9]+', filename).group())
 
 def lesson_name(filename):
@@ -50,6 +62,35 @@ def lesson_name(filename):
     [ind.end(0) for ind in re.finditer('Lesson[0-9]+',filename)]
     return filename[ind.end(0)+1:].split('.')[0].replace('_',' ')
 
+def module_name(filename):
+    print filename
+    return re.search('module[0-9]+_([a-zA-Z_]+)',filename).group(1).replace("_"," ");
+
+def sort_by_module(filenames):
+    '''
+    Takes all of the filenames found and organizes them based on which module
+    they are from
+    '''
+    module_dict = {}
+    for i in filenames:
+        name = module_name(i)
+	if name in module_dict:
+	    module_dict[name].append(i)
+	else:
+	    module_dict[name] = [i]
+    return module_dict
+
+def sort_module_list(modules):
+     '''
+     Takes a dictionary of modules and sorts all of the lessons within it 
+     by their order
+     '''
+     sorted_modules = {}
+     for i in modules:
+         sorted_modules[i] = sorted(modules[i], key=lesson_id)
+
+     return sorted_modules
+
 
 def nb_url(filename):
     '''This isn't really useful anymore, but if your file name has 
@@ -61,22 +102,25 @@ def nb_url(filename):
     raw_url = bb_url(quote(quote(filename))) 
     return 'http://nbviewer.ipython.org/urls/{}'.format(raw_url)
 
-def write_readme(nblist, fo):
+def write_readme(modules, fo):
     '''
     for each notebook in nblist, write out the formatted nbviewer links
     to the file specified by fo (README.md) and prepend the header string
     specified above
     '''
     fo.write('{}\n'.format(header)) 
-    for nb in nblist:
-        name = lesson_name(nb)
-        url = nb_url(nb)
-        fo.write('{}\n'.format(format_item(name=name, url=url)))
+    for mod in modules:
+        fo.write('{}\n'.format(format_module(name=mod)))
+	for nb in modules[mod]:
+        	name = lesson_name(nb)
+        	url = nb_url(nb)
+        	fo.write('{}\n'.format(format_item(name=name, url=url)))
 
 def main():
     nblist = sorted(notebooks(), key=lesson_id) #get list of notebooks sorted by leading number
+    modules = sort_module_list(sort_by_module(notebooks()))
     with open('README.md', 'w') as fo:
-        write_readme(nblist, fo)
+        write_readme(modules, fo)
 
 if __name__ == '__main__':
     main()
